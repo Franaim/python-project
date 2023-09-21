0c976a997af501297ad767ba459e4242bb638d64
franaim <franmenardi.dibujo@gmail.com>
2023-09-21 17:34:52 +0200

    Improve generate_string function for readability

--- run.py
+++ run.py
@@ -54,11 +54,17 @@ def generate_string(num_words):
     """
     Based on the number entered by the user, it generates random strings from the lists of nouns and adjetives
     """
+    title_words = []
+    adjectives_lists = [adjectives1, adjectives2]
+    number_of_adjectives = num_words - 1
+    for n in range(number_of_adjectives):
+        title_words.append(random.choice(adjectives_lists.pop()))
+    title_words.append(random.choice(nouns))
+    return ' '.join(title_words).capitalize()
+    
+"""
     if num_words == 1:
-        noun = random.choice(nouns)
-        noun = noun.capitalize()
-        artwork_title = f"{noun}"
-        return artwork_title
+        return random.choice(nouns)
     elif num_words == 2:
         adj1 = random.choice(adjectives1)
         adj1 = adj1.capitalize()
@@ -72,33 +78,36 @@ def generate_string(num_words):
         noun = random.choice(nouns)
         artwork_title = f"{adj1} {adj2} {noun}"
         return artwork_title
+    """
     
 print("Welcome to the artwork title generator\n")
 print("Please enter the number of words you'd like your artwork title to have.")
 print("It should be a number between 1 and 3.\n")
 
-def generate_title():
+def generate_title(artwork_title):
     """
     Once the user has entered a number and, based on it, a string has been made randomly, it prints a message
     including the artwork title that has been generated. In this case, it checks if the number entered is 1
     or if it's 2 or 3 so that the word 'word' isn't plural when referring to 1. 
     """
-    num_words = get_integer_input()
-    artwork_title = generate_string(num_words)
-
+    num_words = len(artwork_title.split(" "))
     if num_words == 1:
         message = f"\nYour artwork has {num_words} word: '{artwork_title}'\n"
         return(message)
     else:
-        message =  f"\nYour artwork has {num_words} words: '{generate_string(num_words)}'\n"
+        message =  f"\nYour artwork has {num_words} words: '{artwork_title}'\n"
         return(message)
 
 # Calls the generate_title function
-generate_title_result = generate_title()
+artwork_title = generate_string(get_integer_input())
+generate_title_result = generate_title(artwork_title)
 
 # Prints the result for the user
 print(generate_title_result)
+print()
+print(artwork_title)
 
+"""
 def string_convert(string):
     """
     Turns the string generated and turns it into a list of words that can be then stored
@@ -107,5 +116,5 @@ def string_convert(string):
     return li
 
 print(string_convert(generate_title_result))
-
+"""
 
