f0a37fa38a5e41e6b23cfb1c2c59589050791da0
franaim <franmenardi.dibujo@gmail.com>
2023-09-21 11:08:05 +0200

    Add comments

--- run.py
+++ run.py
@@ -28,9 +28,9 @@ def get_integer_input():
     """
     while True:
         user_input = input("Enter a number here: ")
-        if user_input.strip() and user_input.isdigit():
+        if user_input.strip() and user_input.isdigit(): # Check that the input exists and its a number
             user_input = int(user_input) # Convert the input to an integer
-            if user_input <= 3:
+            if user_input <= 3: # Make sure that the input is not bigger than 3
                 return user_input
             else:
                 print("It must be a number between 1 and 3.")
@@ -46,7 +46,7 @@ def generate_title():
     print("Please enter the number of words you'd like your artwork title to have.")
     print("It should be a number between 1 and 3.\n")
 
-    # Get the number of words from the user
+    # Get the number of words from the user and return a message if the input is a number between 1 and 3
     num_words = get_integer_input()
     if num_words == 1:
         print(f"Your artwork title will have {num_words} word.\n")
