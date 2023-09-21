f066f8b0ec142c0a0192a376b9c22a33c91a502b
franaim <franmenardi.dibujo@gmail.com>
2023-09-21 11:28:38 +0200

    More comments for better readability

--- run.py
+++ run.py
@@ -24,13 +24,19 @@ print()
 # Function to get user input as an integer
 def get_integer_input():
     """
-    Validate the number entered in the generate_title function
+    Validates the number entered in the generate_title function.
+    The while loop keeps requesting a number until a valid input is entered.
+    Strip and isdigit methods check that the input exists and it's a number. Then if this is correct, it turns it
+    into an integer. Once done, it makes sure that the integer is not bigger than 3. If it is, it prints a message
+    asking for a smaller number. If it's not, it returns the input and breaks the loop. If, at the beginning of
+    the loop, the input entered isn't a number or is empty a message is printed into the console requesting a 
+    valid number.
     """
     while True:
         user_input = input("Enter a number here: ")
-        if user_input.strip() and user_input.isdigit(): # Check that the input exists and its a number
-            user_input = int(user_input) # Convert the input to an integer
-            if user_input <= 3: # Make sure that the input is not bigger than 3
+        if user_input.strip() and user_input.isdigit():
+            user_input = int(user_input)
+            if user_input <= 3:
                 return user_input
             else:
                 print("It must be a number between 1 and 3.")
@@ -40,13 +46,13 @@ def get_integer_input():
 # Define the generate_title function
 def generate_title():
     """
-    Get number of words for the artwork's title
+    Gets number of words for the artwork's title.
+    If the input entered is a number between 1 and 3 as requested, a messaged is printed including this number.
     """
     print("Welcome to the artwork title generator\n")
     print("Please enter the number of words you'd like your artwork title to have.")
     print("It should be a number between 1 and 3.\n")
 
-    # Get the number of words from the user and return a message if the input is a number between 1 and 3
     num_words = get_integer_input()
     if num_words == 1:
         print(f"Your artwork title will have {num_words} word.\n")
