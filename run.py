30c52762bf1da818182b65fbf6880d8b1dc7081a
franaim <franmenardi.dibujo@gmail.com>
2023-09-21 14:41:13 +0200

    Change structure of functions so I can take the number entered and the title generated separately

--- run.py
+++ run.py
@@ -24,26 +24,26 @@ print()
 
 # Lists of nouns and adjectives to generate the artwork title
 nouns = ["aurora", "object", "panorama", "elixir", "chair", "mirror", "facade", "phenomenon", "resonance", "memento", "artifact"]
-adjectives1 = ["enigmatic", "expressive", "dynamic", "political", "honest", "provocative", "real", "cruel", "truthful"]
-adjectives2 = ["ivory", "cerulean", "golden", "indigo", "unique", "Japanese", "Maltese", "Eastern", "Western", "southern", "blood-red"]
+adjectives1 = ["enigmatic", "expressive", "dynamic", "political", "honest", "provocative", "real", "cruel", "truthful", "world's"]
+adjectives2 = ["ivory", "cerulean", "golden", "indigo", "unique", "Japanese", "Maltese", "Eastern", "Western", "southern", "blood-red", "dystopian"]
 
 
 # Function to get user input as an integer
 def get_integer_input():
     """
-    Validates the number entered in the generate_title function.
+    Gets the number entered by the user.
     The while loop keeps requesting a number until a valid input is entered.
     Strip and isdigit methods check that the input exists and it's a number. Then if this is correct, it turns it
-    into an integer. Once done, it makes sure that the integer is not bigger than 3. If it is, it prints a message
-    asking for a smaller number. If it's not, it returns the input and breaks the loop. If, at the beginning of
-    the loop, the input entered isn't a number or is empty a message is printed into the console requesting a 
+    into an integer. Once that's done, it makes sure that the integer is not bigger than 3. If it is, it prints a 
+    message asking for a smaller number. If it's not, it returns the input and breaks the loop. If, at the beginning 
+    of the loop, the input entered isn't a number or is empty a message is printed into the console requesting a 
     valid number.
     """
     while True:
         user_input = input("Enter a number here: ")
         if user_input.strip() and user_input.isdigit():
             user_input = int(user_input)
-            if user_input <= 3:
+            if 1 <= user_input <= 3:
                 return user_input
             else:
                 print("It must be a number between 1 and 3.")
@@ -72,26 +72,30 @@ def generate_string(num_words):
         noun = random.choice(nouns)
         artwork_title = f"{adj1} {adj2} {noun}"
         return artwork_title
+    
+print("Welcome to the artwork title generator\n")
+print("Please enter the number of words you'd like your artwork title to have.")
+print("It should be a number between 1 and 3.\n")
 
-
-# Defines the generate_title function
 def generate_title():
     """
-    Gets number of words for the artwork's title.
-    If the input entered is a number between 1 and 3 as requested, a messaged is printed including this number.
+    Once the user has entered a number and, based on it, a string has been made randomly, it prints a message
+    including the artwork title that has been generated. In this case, it checks if the number entered is 1
+    or if it's 2 or 3 so that the word 'word' isn't plural when referring to 1. 
     """
-    print("Welcome to the artwork title generator\n")
-    print("Please enter the number of words you'd like your artwork title to have.")
-    print("It should be a number between 1 and 3.\n")
-
     num_words = get_integer_input()
+    artwork_title = generate_string(num_words)
+
     if num_words == 1:
-        print(f"Your artwork has {num_words} word:\n")
-        print(f"{generate_string(num_words)}\n")
+        message = f"\nYour artwork has {num_words} word: '{artwork_title}'\n"
+        return(message)
     else:
-        print(f"Your artwork has {num_words} words:\n")
-        print(f"{generate_string(num_words)}\n")
+        message =  f"\nYour artwork has {num_words} words: '{generate_string(num_words)}'\n"
+        return(message)
 
+# Calls the generate_title function
+generate_title_result = generate_title()
 
-# Call the generate_title function
-generate_title()
+# Prints the result for the user
+print(generate_title_result)
+
